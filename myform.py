0ccca6704795223ce69eeefcbc7ce2952f1df1f1
aremneva <remnevaa0@gmail.com>
2021-04-15 09:59:18 +0300

    fixed json

--- myform.py
+++ myform.py
@@ -2,9 +2,12 @@ from bottle import post, request
 import re
 import pdb
 import json
+import os
 
 @post('/home', method='post')
+
 def my_form():
+    questions={}
     regex_email=(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)")
     question=request.forms.get('QUEST')
     mail = request.forms.get('ADRESS')
@@ -18,13 +21,15 @@ def my_form():
         if not re.search(regex_email,mail):
             return "Incorrect email!"
         else:
-            questions={}
-            questions[mail]=question
-            with open('data.txt', 'a+') as f: #открытие файла для записи
+            if os.stat('data.txt').st_size !=0:
+                with open('data.txt') as fr: #открытие файла для чтения
+                    questions=json.load(fr)
+                fr.close()
+            with open('data.txt', 'w') as f: #открытие файла для записи
+                questions[mail]=question
                 json.dump(questions, f)
-                f.write('\n') 
-                f.close()
-            
+                f.write("\n")
+            f.close()
             #pdb.set_trace()
             return "Thanks! The answer will be sent to the mail %s" % mail
 
