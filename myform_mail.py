596766a2bf56d060c4ffc2b3bd74c566a1ef29dc
Ann <anastasia.balabckina@gmail.com>
2021-04-29 10:33:45 +0300

    Добавление новых неверных адресов и отладка

--- myform_mail.py
+++ myform_mail.py
@@ -3,7 +3,7 @@ import re
 class mailCheck:
 
     def match(mail):
-        regex = re.compile('^[A-Za-z0-9\._]+@[A-Za-z0-9]+(\.[A-Za-z]+)+$')
+        regex = re.compile('^[A-Za-z0-9\._]+@[A-Za-z0-9]+(\.[A-Za-z]{2,3})+$')
         return re.match(regex,mail) is not None
 
 
--- utest_assertFalse.py
+++ utest_assertFalse.py
@@ -3,11 +3,14 @@ import unittest, re, myform_mail
 class AssertFalseTest(unittest.TestCase):
 
     def test_realEmails(self):
-        list_mail_cor = ['some@mail', 'some@mail.', '@mail.ru', 'incorrectMail@@mailCheck.point']
-        
+        list_mail_cor = ['some@mail', 'some@mail.', '@mail.ru',
+                         'incorrectMail@@mailCheck.point', 'user@mail.ru@',
+                         '@user@mail.ru', 'username@mail.n', 'user@mail.russ'
+                         'user.@mail.ru', 'user@mail..ru', 'user@mail.ru.']
         for mail in list_mail_cor:
             self.assertFalse(myform_mail.mailCheck.match(mail))
 
 if __name__ == '__main__':
     unittest.main()
+
 
